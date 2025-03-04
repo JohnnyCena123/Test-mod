8da306c68a0f59e6b0f92bf29dcef886b61e302c
JohnnyCena123 <eitanmiller7@gmail.com>
2025-03-04 10:44:01 +0200

    Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MyMenuLayer, MenuLayer) {
+
+void onPlayButton {
+FLAlertLayer::create("Hello world", 
+"Hello from my <cp>mobile</c> mod!", 
+"Close"
+)->show();
+}
+
+};
